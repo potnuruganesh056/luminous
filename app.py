0e991f531eb0404b9cc81184d46566dce43ff702
POTNURU GANESH <137292636+Potnuru-Ganesh0805@users.noreply.github.com>
2025-09-16 19:09:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1620,6 +1620,8 @@ def link_github():
     # If not linked, proceed to GitHub
     redirect_uri = url_for('link_authorize_github', _external=True)
     return github_link.authorize_redirect(redirect_uri)
+
+@app.route('link/google/callback')
 @login_required
 def link_authorize_google():
     """Handles the callback after the user authorizes Google linking."""
