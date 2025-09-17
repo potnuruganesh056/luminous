404826cfd67d80dcbb8e352a06b29621c767dc80
POTNURU GANESH <137292636+Potnuru-Ganesh0805@users.noreply.github.com>
2025-09-17 15:44:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1791,4 +1791,4 @@ if __name__ == '__main__':
     # generate_analytics_data()
     run_mqtt_thread()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
